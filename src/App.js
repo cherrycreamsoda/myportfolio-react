2d8e1f3f65466aa4dac9cf173ffe83aedc15bbac
cherrycreamsoda <hamzazain2003@gmail.com>
2025-02-18 01:08:31 +0500

    add FloatingBlobs component for a BLOBY! animated background effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import GridPattern from "./components/GridPattern";
+import { FloatingBlobs } from "./components/FloatingBlobs";
 
 export default function App() {
   return (
     <>
       <GridPattern />
+      <FloatingBlobs />
       <main>
         <h1>Hello World.</h1>
       </main>
